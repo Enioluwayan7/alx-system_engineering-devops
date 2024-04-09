812a3a4a48e83356cc389818911e79db0f1e9da0
Enioluwayan7 <Olabanjienioluwayan@gmail.com>
2024-04-09 11:57:55 +0000

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" function that queries the Reddit API and returns the number of 
+subscribers (not active users, total subscribers) for a given subreddit"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+	""" Return the number of subscribers of a given subreddit"""
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {"User-Agent": "Mozilla/5.0"}
+	response = requests.get(url, headers=headers, allow_redirects=False)
+	if response.status_code == 200:
+		data = reponse.json()
+		subscribers = data['data']['subscribers']
+		return subscribers
+	else:
+		return 0
