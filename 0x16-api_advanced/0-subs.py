4c8f76b3ff2adc4618fb274692194239094f6da6
Enioluwayan7 <olabanjienioluwayan@gmail.com>
2024-06-20 08:53:56 +0000

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""Return the total number of subscribers on a given subreddit."""
+	"""
+	Return the total number of subscribers on a given subreddit
+	"""
+
 	url = f"https://www.reddit.com/r/{subreddit}/about.json"
 	headers = {"user.agent": 'request'}
 	response = requests.get(url, headers=headers, allow_redirects=False)
